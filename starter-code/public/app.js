061f2b587c491bc884fa13267a0c1ed554af308e
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-29 21:43:17 -0700

    adds server.js code attempts

--- starter-code/public/app.js
+++ starter-code/public/app.js
@@ -1,4 +1,18 @@
 console.log('app.js connected');
 $(document).ready(function(){
   console.log('DOM ready');
+
+$('#guess-number-form').on('submit', function(event){
+  event.preventDefault();
+  
+  $.ajax({
+    method: 'GET',
+    url: "/pickanumber",
+    dataType: 'text',
+    success: onSuccess
+  });
+
+  function onSuccess(text) {
+    $("#guess-number-form").html("success");
+  };
 });
--- starter-code/server.js
+++ starter-code/server.js
@@ -22,16 +22,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // ROUTES
 // Root Route
-
+app.get('/', function (req, res) {
+  res.sendFile('views/index.html' , { root : __dirname});
+});
 
 // Gallery View Route
-
+app.get('/', function (req, res) {
+  res.sendFile('views/art-gallery.html' , { root : __dirname});
+});
 
 // The Number Guessing Game
-
+var targetNum = 7;
+app.get("/pickanumber", function (request, response) {
+  var number = request.query.number;
+  if (number > targetNum) {
+  response.send("Too High");
+}
+  else if (number < targetNum) {
+    response.send("Too Low");
+  }
+  else if (number == targetNum) {
+    response.send("Nailed it!");
+  }
+});
 
 // Gallery
+app.get("/art-gallery", function (request, response) {
+  response.send(json);
+});
 
+// var artworksArray = {
+//   title:
+//   artist:
+//   description:
+// };
 
 // SERVER START
 var port = 3000;
